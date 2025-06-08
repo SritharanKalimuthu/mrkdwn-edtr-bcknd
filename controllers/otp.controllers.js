65756c39ffd2c1f1967529ca84ffa35f4f8238dc
Sridhar <sritharan@ysquaretechnology.com>
2025-06-08 15:50:25 +0530

    Fixed Token issue

--- controllers/otp.controllers.js
+++ controllers/otp.controllers.js
@@ -32,18 +32,18 @@ export const verifyOtp = async (req, res) => {
         const accessToken = await generateAccessToken(user);
         const refreshToken = await generateRefreshToken(user);
 
-        res.cookie('accessToken', accessToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 15 * 60 * 1000, 
-        });
-
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            secure: true, 
+            sameSite: 'None', 
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        res.cookie('accessToken', accessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None',
+            maxAge: 15 * 60 * 1000,
         });
 
         res.status(200).json({
